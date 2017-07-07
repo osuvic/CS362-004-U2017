8cadbb6fe9938bf2ad36779b34ceaa364f2ba070
osuvic <wangv@oregonstate.edu>
2017-07-07 14:10:30 +0700

    Add files via upload

--- projects/wangv/dominion/dominion_helpers.h
+++ projects/wangv/dominion/dominion_helpers.h
@@ -5,11 +5,15 @@
 
 int drawCard(int player, struct gameState *state);
 int updateCoins(int player, struct gameState *state, int bonus);
-int discardCard(int handPos, int currentPlayer, struct gameState *state, 
+int discardCard(int handPos, int currentPlayer, struct gameState *state,
 		int trashFlag);
 int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
-int cardEffect(int card, int choice1, int choice2, int choice3, 
+int cardEffect(int card, int choice1, int choice2, int choice3,
 	       struct gameState *state, int handPos, int *bonus);
-
+int cardEffectSmithy(int currentPlayer, struct gameState *state, int handPos);
+int cardEffectVillage(int currentPlayer, struct gameState *state, int handPos);
+int cardEffectGreatHall(int currentPlayer, struct gameState *state, int handPos);
+int cardEffectOutpost(int currentPlayer, struct gameState *state, int handPos);
+int cardEffectAdventurer(int currentPlayer, struct gameState *state, int drawntreasure, int cardDrawn, int *temphand, int size);
 #endif
