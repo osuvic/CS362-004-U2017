95c3e2896baef6340a3c93ac8420bc63d9b171d6
osuvic <wangv@oregonstate.edu>
2017-07-17 12:34:43 +0700

    Add files via upload

---
+++ projects/wangv/dominion/cardtest3.c
@@ -0,0 +1,58 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+int assertFunc(int actual, int expected)
+{
+  if (actual == expected){
+    //printf("Assert confirmed.\n");
+    return 0;
+  }
+  else {
+    printf("Error! Assert failed!\n");
+    return -1;
+  }
+
+}
+
+int main(){
+
+  int i;
+
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+
+  int currentPlayer;
+  int expHandCount;
+
+  int temphand[MAX_HAND];
+  int drawntreasure = 0;
+  int cardDrawn = 0;
+
+  struct gameState G;
+
+  initializeGame(2, k, 1, &G);//Initialize game with 2 players
+
+  printf("TESTING Card Adventurer:\n");
+
+  currentPlayer = whoseTurn(&G);
+  expHandCount = 5;
+
+  printf("Actual handcount = %d, expected = %d\n", G.handCount[currentPlayer], expHandCount);
+
+
+  cardEffectAdventurer(currentPlayer, &G, drawntreasure, cardDrawn, temphand, MAX_HAND);
+  printf("Playing Adventurer...\n");
+
+  expHandCount = 7;
+  printf("Actual handcount = %d, expected = %d\n", G.handCount[currentPlayer], expHandCount);
+
+  if (assertFunc(G.handCount[currentPlayer], expHandCount) == 0){
+    printf("All tests passed!\n");
+  }
+
+  return 0;
+}
---
+++ projects/wangv/dominion/unittest4.c
@@ -0,0 +1,60 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+int main(){
+
+  int i;
+
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+
+  int tempCount;
+
+  int actualCount;
+
+  struct gameState G;
+
+  initializeGame(2, k, 1, &G);//Initialize game with 2 players
+
+  printf("TESTING supplyCount():\n");
+
+  printf("Initial Count of Adventurer is %d\n", supplyCount(adventurer, &G));
+  for (i = 0; i < 12; i++)
+  {
+    tempCount = supplyCount(adventurer, &G);
+
+    gainCard(7, &G, 0, 0);
+    actualCount = supplyCount(adventurer, &G);
+    if(tempCount > 0){
+      tempCount = tempCount - 1;
+    }
+    //int gainCard(int supplyPos, struct gameState *state, int toFlag, int player)
+
+    assert(tempCount == actualCount);
+    printf("Actual count = %d, expected count = %d\n", actualCount, tempCount);
+  }
+
+  printf("\nInitial Count of Province is %d\n", supplyCount(province, &G));
+  for (i = 0; i < 10; i++)
+  {
+    tempCount = supplyCount(province, &G);
+
+    gainCard(3, &G, 0, 0);
+    actualCount = supplyCount(province, &G);
+    if(tempCount > 0){
+      tempCount = tempCount - 1;
+    }
+
+    assert(tempCount == actualCount);
+    printf("Actual count = %d, expected count = %d\n", actualCount, tempCount);
+  }
+
+
+  return 0;
+
+}
+
