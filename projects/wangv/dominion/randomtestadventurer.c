a33d570d8801aa8e9db4dbf826e67ea3d8f686a4
osuvic <wangv@oregonstate.edu>
2017-07-31 12:45:29 +0700

    Assignment 4 files

---
+++ projects/wangv/dominion/randomtestadventurer.c
@@ -0,0 +1,58 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+int checkCardEffectAdventurer(int p, struct gameState *post, int i)
+{
+  struct gameState pre;
+  int drawntreasure = i;
+  int carddrawn = i;
+  int temphand[MAX_HAND];
+  int size = i;
+
+  pre.handCount[p] = i;
+
+  printf("Playing Adventurer Card...\n\n");
+  cardEffectAdventurer(p, post, drawntreasure, carddrawn, temphand, size);
+
+  printf("Asserting handCount...\n");
+  assertFunc(pre.handCount[p], post->handCount[p]);
+}
+
+assertFunc(int actual, int expected)
+{
+  if (actual == expected){
+    printf("Assert confirmed.\n\n");
+    return 0;
+  }
+  else {
+    printf("Error! Assert failed!\n\n");
+    return -1;
+  }
+}
+
+int main () {
+
+  int n, p, handCount;
+
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+
+  struct gameState G;
+
+  initializeGame(2, k, 1, &G);
+
+  printf ("Testing cardEffectAdventurer.\n");
+
+  printf ("RANDOM TESTS.\n");
+
+  for (n = -1; n < 9; n++) {
+    G.handCount[p] = floor(Random() * n);
+    checkCardEffectAdventurer(p, &G, n);
+  }
+
+  return 0;
+}
---
+++ projects/wangv/dominion/randomtestcard2.c
@@ -0,0 +1,69 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+int checkCardEffectVillage(int p, struct gameState *post, int i)
+{
+  struct gameState pre;
+
+  pre.handCount[p] = i;
+  pre.numActions = i;
+
+  printf("pre handcount = %d, post handcount = %d\n", pre.handCount[p],post->handCount[p]);
+  printf("pre numActions = %d, post numActions = %d\n\n", pre.numActions, post->numActions);
+
+  printf("Playing Smithy Card...\n\n");
+  cardEffectVillage(p, post, 0);
+
+  printf("pre handcount = %d, post handcount = %d\n", pre.handCount[p],post->handCount[p]);
+  printf("pre numActions = %d, post numActions = %d\n\n", pre.numActions, post->numActions);
+
+  pre.handCount[p] = pre.handCount[p] + 1;
+  pre.numActions = pre.numActions + 2;
+
+  printf("Asserting handcount...\n\n");
+  assertFunc(pre.handCount[p], post->handCount[p]);
+
+  printf("Asserting numActions...\n\n");
+  assertFunc(pre.numActions, post->numActions);
+}
+
+assertFunc(int actual, int expected)
+{
+  if (actual == expected){
+    printf("Assert confirmed.\n");
+    return 0;
+  }
+  else {
+    printf("Error! Assert failed!\n");
+    return -1;
+  }
+}
+
+int main () {
+
+  int n, p, handCount;
+
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+
+  struct gameState G;
+
+  initializeGame(2, k, 1, &G);
+
+  printf ("Testing cardEffectVillage.\n");
+
+  printf ("RANDOM TESTS.\n");
+
+  for (n = -1; n < 9; n++) {
+    G.handCount[p] = floor(Random() * n);
+    G.numActions = floor(Random() * n);
+
+    checkCardEffectVillage(p, &G, n);
+  }
+
+  return 0;
+}
