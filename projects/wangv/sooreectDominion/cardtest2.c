5526a3d997b130aa1b6efc15ca11b016b512ba99
osuvic <wangv@oregonstate.edu>
2017-08-07 13:39:16 +0700

    Assignment-5 files

---
+++ projects/wangv/sooreectDominion/cardtest2.c
@@ -0,0 +1,59 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+int assertFunc(int actual, int expected)
+{
+  if (actual == expected){
+    //printf("Assert confirmed.\n");
+    return 0;
+  }
+  else {
+    printf("Error! Assert failed!\n");
+    return -1;
+  }
+
+}
+
+int main(){
+
+  int i;
+
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+
+  int currentPlayer;
+  int expHandCount;
+
+  struct gameState G;
+
+  initializeGame(2, k, 1, &G);//Initialize game with 2 players
+
+  printf("TESTING Card Smithy:\n");
+
+  currentPlayer = whoseTurn(&G);
+
+  expHandCount = 5;
+
+  assertFunc(G.handCount[currentPlayer], expHandCount);
+  printf("Actual handcount = %d, expected = %d\n", G.handCount[currentPlayer], expHandCount);
+
+  smithyCard(&G, 0, currentPlayer);
+  printf("Playing Smithy...\n");
+
+  expHandCount = 7;
+
+  assertFunc(G.handCount[currentPlayer], expHandCount);
+  printf("Actual handcount = %d, expected = %d\n", G.handCount[currentPlayer], expHandCount);
+
+  if (assertFunc(G.handCount[currentPlayer], expHandCount) == 0){
+    printf("All tests passed!\n");
+  }
+
+
+  return 0;
+}
+
---
+++ projects/wangv/sooreectDominion/unittest3.c
@@ -0,0 +1,80 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+int main(){
+
+  int i;
+
+  int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+
+  int currentPlayer;
+
+  struct gameState G;
+
+  initializeGame(2, k, 1, &G);//Initialize game with 2 players
+
+  printf("TESTING whoseTurn(): 2 Players\n");
+  for (i = 0; i < 12; i++){
+    currentPlayer = whoseTurn(&G);
+    printf("Whose turn is now player %d\n", G.whoseTurn);
+
+    if (currentPlayer < (G.numPlayers - 1)){
+      currentPlayer = currentPlayer + 1;//Still safe to increment
+    }
+    else{
+      currentPlayer = 0;//Max player has been reached, loop back around to player 1
+    }
+
+    endTurn(&G);
+    assert(G.whoseTurn == currentPlayer);
+  }
+
+  printf("All tests passed!\n\n");
+
+
+  initializeGame(3, k, 1, &G);//Initialize game with 3 players
+
+  printf("TESTING whoseTurn(): 3 Players\n");
+  for (i = 0; i < 12; i++){
+    currentPlayer = whoseTurn(&G);
+    printf("Whose turn is now player %d\n", G.whoseTurn);
+
+    if (currentPlayer < (G.numPlayers - 1)){
+      currentPlayer = currentPlayer + 1;//Still safe to increment
+    }
+    else{
+      currentPlayer = 0;//Max player has been reached, loop back around to player 1
+    }
+
+    endTurn(&G);
+    assert(G.whoseTurn == currentPlayer);
+  }
+  printf("All tests passed!\n\n");
+
+
+  initializeGame(4, k, 1, &G);//Initialize game with 4 players
+
+  printf("TESTING whoseTurn(): 4 Players\n");
+  for (i = 0; i < 12; i++){
+    currentPlayer = whoseTurn(&G);
+    printf("Whose turn is now player %d\n", G.whoseTurn);
+
+    if (currentPlayer < (G.numPlayers - 1)){
+      currentPlayer = currentPlayer + 1;//Still safe to increment
+    }
+    else{
+      currentPlayer = 0;//Max player has been reached, loop back around to player 1
+    }
+
+    endTurn(&G);
+    assert(G.whoseTurn == currentPlayer);
+  }
+  printf("All tests passed!\n");
+
+  return 0;
+}
